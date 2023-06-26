03b5c7522035e5e8edd337cc3c53c4306adcf1b4
IT042301 <it042301@IT042301s-MacBook-Pro.local>
2023-06-26 12:59:48 +0530

    form updated

--- mikalto-frontend/js/script.js
+++ mikalto-frontend/js/script.js
@@ -8,23 +8,21 @@ const submit = document.querySelector(".submit-btn");
 
 let checkInDate;
 let checkOutDate;
-window.onload=function(){
-   const todayDate = new Date().toISOString().split('T')[0];
-   checkInDate = todayDate;
-  document.getElementsByName("check-in-date")[0].setAttribute('min', checkInDate);
+window.onload = function () {
+  const todayDate = new Date().toISOString().split("T")[0];
+  checkInDate = todayDate;
+  document
+    .getElementsByName("check-in-date")[0]
+    .setAttribute("min", checkInDate);
 
   const checkOutDate = document.getElementsByClassName("check-out-date");
-  checkOutDate.value = todayDate.toISOString().slice(0, 10)
+  checkOutDate.value = todayDate.toISOString().slice(0, 10);
   console.log(checkOutDate.value);
-      checkOutDate.setAttribute("min", todayDate.toISOString().slice(0, 10));
+  checkOutDate.setAttribute("min", todayDate.toISOString().slice(0, 10));
 
   // checkOutDate = todayDate;
   // document.getElementsByName("check-out-date")[0].setAttribute('min', checkOutDate);
-
-}
-
-
-
+};
 
 async function heroSection() {
   try {
@@ -34,8 +32,7 @@ async function heroSection() {
     document.querySelector(
       ".hero-section"
     ).style.backgroundImage = `url(${data.heroSection.image})`;
-  }
-  catch (error) {
+  } catch (error) {
     console.log("error in hero section");
   }
 }
@@ -43,13 +40,14 @@ heroSection();
 
 async function welcomeSection() {
   try {
-    let data = await fetch("http://localhost:9400/welcomeSection").then((data) => data.json());
+    let data = await fetch("http://localhost:9400/welcomeSection").then(
+      (data) => data.json()
+    );
     document.querySelector(".house-image").src = data.welcomeSection.houseImage;
     document.querySelector(".pool-image").src = data.welcomeSection.poolImage;
     document.querySelector(".house2-image").src =
       data.welcomeSection.house2Image;
-  }
-  catch (error) {
+  } catch (error) {
     console.log("error in welcome section");
   }
 }
@@ -57,30 +55,49 @@ welcomeSection();
 
 async function roomsSection() {
   try {
-    let data = await fetch("http://localhost:9400/roomsSection").then((data) => data.json());
-    document.querySelector(".standard-room").src = data.roomsSection.standardRoom.img;
-    document.querySelector(".standard-room-price").innerHTML = data.roomsSection.standardRoom.price;
-    document.querySelector(".standard-bed").innerHTML = data.roomsSection.standardRoom.bed;
-    document.querySelector(".standard-capacity").innerHTML = data.roomsSection.standardRoom.capacity;
-    document.querySelector(".standard-size").innerHTML = data.roomsSection.standardRoom.roomSize;
-    document.querySelector(".standard-view").innerHTML = data.roomsSection.standardRoom.view;
+    let data = await fetch("http://localhost:9400/roomsSection").then((data) =>
+      data.json()
+    );
+    document.querySelector(".standard-room").src =
+      data.roomsSection.standardRoom.img;
+    document.querySelector(".standard-room-price").innerHTML =
+      data.roomsSection.standardRoom.price;
+    document.querySelector(".standard-bed").innerHTML =
+      data.roomsSection.standardRoom.bed;
+    document.querySelector(".standard-capacity").innerHTML =
+      data.roomsSection.standardRoom.capacity;
+    document.querySelector(".standard-size").innerHTML =
+      data.roomsSection.standardRoom.roomSize;
+    document.querySelector(".standard-view").innerHTML =
+      data.roomsSection.standardRoom.view;
 
-    document.querySelector(".luxary-room").src = data.roomsSection.luxaryRoom.img;
-    document.querySelector(".luxary-room-price").innerHTML = data.roomsSection.luxaryRoom.price;
-    document.querySelector(".luxary-bed").innerHTML = data.roomsSection.luxaryRoom.bed;
-    document.querySelector(".luxary-capacity").innerHTML = data.roomsSection.luxaryRoom.capacity;
-    document.querySelector(".luxary-size").innerHTML = data.roomsSection.luxaryRoom.roomSize;
-    document.querySelector(".luxary-view").innerHTML = data.roomsSection.luxaryRoom.view;
+    document.querySelector(".luxary-room").src =
+      data.roomsSection.luxaryRoom.img;
+    document.querySelector(".luxary-room-price").innerHTML =
+      data.roomsSection.luxaryRoom.price;
+    document.querySelector(".luxary-bed").innerHTML =
+      data.roomsSection.luxaryRoom.bed;
+    document.querySelector(".luxary-capacity").innerHTML =
+      data.roomsSection.luxaryRoom.capacity;
+    document.querySelector(".luxary-size").innerHTML =
+      data.roomsSection.luxaryRoom.roomSize;
+    document.querySelector(".luxary-view").innerHTML =
+      data.roomsSection.luxaryRoom.view;
 
-    document.querySelector(".ordinary-room").src = data.roomsSection.ordinaryRoom.img;
-    document.querySelector(".ordinary-room-price").innerHTML = data.roomsSection.ordinaryRoom.price;
-    document.querySelector(".ordinary-bed").innerHTML = data.roomsSection.ordinaryRoom.bed;
-    document.querySelector(".ordinary-capacity").innerHTML = data.roomsSection.ordinaryRoom.capacity;
-    document.querySelector(".ordinary-size").innerHTML = data.roomsSection.ordinaryRoom.roomSize;
+    document.querySelector(".ordinary-room").src =
+      data.roomsSection.ordinaryRoom.img;
+    document.querySelector(".ordinary-room-price").innerHTML =
+      data.roomsSection.ordinaryRoom.price;
+    document.querySelector(".ordinary-bed").innerHTML =
+      data.roomsSection.ordinaryRoom.bed;
+    document.querySelector(".ordinary-capacity").innerHTML =
+      data.roomsSection.ordinaryRoom.capacity;
+    document.querySelector(".ordinary-size").innerHTML =
+      data.roomsSection.ordinaryRoom.roomSize;
 
-    document.querySelector(".ordinary-view").innerHTML = data.roomsSection.ordinaryRoom.view;
-  }
-  catch (error) {
+    document.querySelector(".ordinary-view").innerHTML =
+      data.roomsSection.ordinaryRoom.view;
+  } catch (error) {
     console.log("error in rooms section");
   }
 }
@@ -88,11 +105,13 @@ roomsSection();
 
 async function activitiesSection() {
   try {
-    let data = await fetch("http://localhost:9400/activitiesSection").then((data) => data.json());
-    document.querySelector(".swimming-pool-image").src = data.activitiesSection.swimmingPool;
+    let data = await fetch("http://localhost:9400/activitiesSection").then(
+      (data) => data.json()
+    );
+    document.querySelector(".swimming-pool-image").src =
+      data.activitiesSection.swimmingPool;
     document.querySelector(".home-image").src = data.activitiesSection.house;
-  }
-  catch (error) {
+  } catch (error) {
     console.log("error in rooms section");
   }
 }
@@ -100,53 +119,76 @@ activitiesSection();
 
 async function experienceSection() {
   try {
-    let data = await fetch("http://localhost:9400/experienceSection").then((data) => data.json());
-    document.querySelector(".massage-image").src = data.experienceSection.massageImage;
-    document.querySelector(".window-view").src = data.experienceSection.windowView;
-    document.querySelector(".soaps-image").src = data.experienceSection.soapsImage;
-    document.querySelector(".wild-image").src = data.experienceSection.wildImage;
-
-  }
-  catch (error) {
-    console.log('error in experience section');
+    let data = await fetch("http://localhost:9400/experienceSection").then(
+      (data) => data.json()
+    );
+    document.querySelector(".massage-image").src =
+      data.experienceSection.massageImage;
+    document.querySelector(".window-view").src =
+      data.experienceSection.windowView;
+    document.querySelector(".soaps-image").src =
+      data.experienceSection.soapsImage;
+    document.querySelector(".wild-image").src =
+      data.experienceSection.wildImage;
+  } catch (error) {
+    console.log("error in experience section");
   }
 }
 experienceSection();
 
 async function helloSection() {
   try {
-    let data = await fetch('http://localhost:9400/helloSection').then((data) => data.json());
-    document.querySelector(".hello-section").style.backgroundImage = `url(${data.helloSection.backgroundImage})`;
-  }
-  catch (error) {
-    console.log('error in hello section');
+    let data = await fetch("http://localhost:9400/helloSection").then((data) =>
+      data.json()
+    );
+    document.querySelector(
+      ".hello-section"
+    ).style.backgroundImage = `url(${data.helloSection.backgroundImage})`;
+  } catch (error) {
+    console.log("error in hello section");
   }
 }
 helloSection();
 
 async function ratingSection() {
   try {
-    let data = await fetch('http://localhost:9400/ratingSection').then((data) => data.json())
-    document.querySelector(".first-customer-image").src = data.ratingSection.firstCustomer.customerImage;
-    document.querySelector(".first-customer-name").innerHTML = data.ratingSection.firstCustomer.customerName;
-    document.querySelector(".first-rating-date").innerHTML = data.ratingSection.firstCustomer.dateOfReview;
-    document.querySelector(".first-customer-head").innerHTML = data.ratingSection.firstCustomer.reviewHead;
-    document.querySelector(".first-customer-review").innerHTML = data.ratingSection.firstCustomer.reviewDetails;
+    let data = await fetch("http://localhost:9400/ratingSection").then((data) =>
+      data.json()
+    );
+    document.querySelector(".first-customer-image").src =
+      data.ratingSection.firstCustomer.customerImage;
+    document.querySelector(".first-customer-name").innerHTML =
+      data.ratingSection.firstCustomer.customerName;
+    document.querySelector(".first-rating-date").innerHTML =
+      data.ratingSection.firstCustomer.dateOfReview;
+    document.querySelector(".first-customer-head").innerHTML =
+      data.ratingSection.firstCustomer.reviewHead;
+    document.querySelector(".first-customer-review").innerHTML =
+      data.ratingSection.firstCustomer.reviewDetails;
 
-    document.querySelector(".second-customer-image").src = data.ratingSection.secondCustomer.customerImage;
-    document.querySelector(".second-customer-name").innerHTML = data.ratingSection.secondCustomer.customerName;
-    document.querySelector(".second-rating-date").innerHTML = data.ratingSection.secondCustomer.dateOfReview;
-    document.querySelector(".second-customer-head").innerHTML = data.ratingSection.secondCustomer.reviewHead;
-    document.querySelector(".second-customer-review").innerHTML = data.ratingSection.secondCustomer.reviewDetails;
+    document.querySelector(".second-customer-image").src =
+      data.ratingSection.secondCustomer.customerImage;
+    document.querySelector(".second-customer-name").innerHTML =
+      data.ratingSection.secondCustomer.customerName;
+    document.querySelector(".second-rating-date").innerHTML =
+      data.ratingSection.secondCustomer.dateOfReview;
+    document.querySelector(".second-customer-head").innerHTML =
+      data.ratingSection.secondCustomer.reviewHead;
+    document.querySelector(".second-customer-review").innerHTML =
+      data.ratingSection.secondCustomer.reviewDetails;
 
-    document.querySelector(".third-customer-image").src = data.ratingSection.thirdCustomer.customerImage;
-    document.querySelector(".third-customer-name").innerHTML = data.ratingSection.thirdCustomer.customerName;
-    document.querySelector(".third-rating-date").innerHTML = data.ratingSection.thirdCustomer.dateOfReview;
-    document.querySelector(".third-customer-head").innerHTML = data.ratingSection.thirdCustomer.reviewHead;
-    document.querySelector(".third-customer-review").innerHTML = data.ratingSection.thirdCustomer.reviewDetails;
-  }
-  catch (error) {
-    console.log('error in rating section');
+    document.querySelector(".third-customer-image").src =
+      data.ratingSection.thirdCustomer.customerImage;
+    document.querySelector(".third-customer-name").innerHTML =
+      data.ratingSection.thirdCustomer.customerName;
+    document.querySelector(".third-rating-date").innerHTML =
+      data.ratingSection.thirdCustomer.dateOfReview;
+    document.querySelector(".third-customer-head").innerHTML =
+      data.ratingSection.thirdCustomer.reviewHead;
+    document.querySelector(".third-customer-review").innerHTML =
+      data.ratingSection.thirdCustomer.reviewDetails;
+  } catch (error) {
+    console.log("error in rating section");
   }
 }
 
@@ -154,13 +196,14 @@ ratingSection();
 
 async function joinUsSection() {
   try {
-    let data = await fetch('http://localhost:9400/joinUsSection').then((data) => data.json());
+    let data = await fetch("http://localhost:9400/joinUsSection").then((data) =>
+      data.json()
+    );
     document.querySelector(".bridge").src = data.joinUsSection.bridgeImage;
     document.querySelector(".cat-image").src = data.joinUsSection.catImage;
     document.querySelector(".dog-image").src = data.joinUsSection.forestImage;
-  }
-  catch (error) {
-    console.log('error in join us section');
+  } catch (error) {
+    console.log("error in join us section");
   }
 }
 
@@ -205,19 +248,24 @@ rightScroll.forEach((element) => {
   });
 });
 
- const form = document.querySelector(".form");
+const form = document.querySelector(".form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-let data = fetch("http://localhost:9400/post-data", {
-  method: "POST",
-  headers: {
-    "Content-type": "data/json; charset=UTF-8",
-  },
-  body : `id=${Date.now()}&checkInDate=${document.querySelector(".check-in-date ").value}&checkOutDate=${document.querySelector(".check-out-date ").value}&noOfAdults=${document.querySelector(".adults-no").value}&noOfchildren=${document.querySelector(".children-no").value}`,
-}).then((res) =>{
-  document.querySelector(".check-in-date ").value = "";
-  document.querySelector(".check-out-date ").value = "";
-  document.querySelector(".adults-no").value = "";
-  document.querySelector(".children-no").value = "";
-})
-})
+  let data = fetch("http://localhost:9400/post-data", {
+    method: "POST",
+    headers: {
+      "Content-type": "data/json; charset=UTF-8",
+    },
+    body: `id=${Date.now()}&checkInDate=${document.querySelector(".check-in-date ").value
+      }&checkOutDate=${document.querySelector(".check-out-date ").value
+      }&noOfAdults=${document.querySelector(".adults-no").value}&noOfchildren=${document.querySelector(".children-no").value
+      }`,
+  }).then((res) => {
+     document.querySelector(".check-in-date ").value = "";
+     document.querySelector(".check-out-date ").value = "";
+     document.querySelector(".adults-no").value = "";
+     document.querySelector(".children-no").value = "";
+  })
+  (alert("form submitted successfully!!"));
+
+}) 
