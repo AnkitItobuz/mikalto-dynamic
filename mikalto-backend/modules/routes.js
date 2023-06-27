3169a92ad18e3ecd54197dd3b4f25c4ba60c575d
IT042301 <it042301@IT042301s-MacBook-Pro.local>
2023-06-27 17:08:15 +0530

    file structure changed

--- mikalto-backend/modules/routes.js
+++ mikalto-backend/modules/routes.js
@@ -16,21 +16,29 @@ export function getRequestData(req) {
     return JSON.stringify({
       // status,
       heroSection: data.heroSection,
+      message: "heroSection data",
+      Sucess: true
     });
   } else if (req.url === "/welcomeSection") {
     return JSON.stringify({
       // status,
       welcomeSection: data.welcomeSection,
+      message: "welcomeSection data",
+      Sucess: true
     });
   } else if (req.url === "/roomsSection") {
     return JSON.stringify({
       // status,
       roomsSection: data.roomsSection,
+      message: "roomsSection data",
+      Sucess: true
     });
   } else if (req.url === "/activitiesSection") {
     return JSON.stringify({
       // status,
       activitiesSection: data.activitiesSection,
+      message: "activitiesSection data",
+      Sucess: true
     });
   } else if (req.url === "/experienceSection") {
     return JSON.stringify({
