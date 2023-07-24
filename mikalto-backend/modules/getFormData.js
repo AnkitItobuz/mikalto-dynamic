63cc19b704bad87966bc252bada5188bc8cb6c0d
IT042301 <it042301@IT042301s-MacBook-Pro.local>
2023-07-24 10:11:18 +0530

    bug fixed

--- mikalto-backend/modules/getFormData.js
+++ mikalto-backend/modules/getFormData.js
@@ -1,8 +1,8 @@
-
+import { parse } from "querystring";
 import { modifyFile } from "./formData.js";
 
 export function formData(req) {
-  
+  if (req.url === "/post-data") {
     let chunks = "";
     req.on("data", (chunk) => {
       chunks = chunk.toString();
@@ -10,11 +10,11 @@ export function formData(req) {
     let parsedData;
     req.on("end", () => {
       parsedData = parse(chunks);
-      console.log(parsedData);
       if (Object.keys(parsedData).length !== 0) {
-        modifyFile(parsedData, "./formData.json");
+        modifyFile(parsedData, "./database/formData.json");
       }
     });
     return JSON.stringify({ parsedData: parsedData });
+  }
   }
 
